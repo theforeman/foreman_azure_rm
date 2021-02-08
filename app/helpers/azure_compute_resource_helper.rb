e550d288745c4ac35aafa98df77202828c8a2e9c
Chris Roberts <chrobert@redhat.com>
2021-02-08 20:43:46 +0000

    Fixes #31837 - Add plugin doc button to main form

--- app/helpers/azure_compute_resource_helper.rb
+++ app/helpers/azure_compute_resource_helper.rb
@@ -10,4 +10,8 @@ module AzureComputeResourceHelper
     end
     selectable_f(f, :url, regions, {}, {:label => _('Azure Region'), :disabled => regions.empty?, :required => true, :help_inline_permanent => load_button_f(f, regions.present?, _("Load Regions")) })
   end
+  def azure_doc_url
+    doc_version = Foreman::Plugin.find(:foreman_azure_rm).version.scan(/\d+\.\d+/).first + '.x'
+    "https://theforeman.org/plugins/foreman_azure/#{doc_version}"
+  end
 end
