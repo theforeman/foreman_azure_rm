2bb74a1f7ff18d3ad05e65fe0c63c0f3ccf2fa1a
Shim Shtein <sshtein@redhat.com>
2024-07-04 17:14:07 +0300

    Fixes #37625 - Fix the plugin to support zeitwerk

--- lib/foreman_azure_rm/engine.rb
+++ lib/foreman_azure_rm/engine.rb
@@ -4,12 +4,12 @@ module ForemanAzureRm
 
     #autoloading all files inside lib dir
     config.eager_load_paths += Dir["#{config.root}/lib"]
-    config.eager_load_paths += Dir["#{config.root}/app/models/concerns/foreman_azure_rm/vm_extensions/"]
+    config.eager_load_paths += Dir["#{config.root}/app/models/concerns/"]
     config.eager_load_paths += Dir["#{config.root}/app/helpers/"]
 
     initializer 'foreman_azure_rm.register_plugin', :before => :finisher_hook do
       Foreman::Plugin.register :foreman_azure_rm do
-        requires_foreman '>= 3.7'
+        requires_foreman '>= 3.13'
         register_gettext
         compute_resource ForemanAzureRm::AzureRm
         parameter_filter ComputeResource, :azure_vm, :tenant, :app_ident, :secret_key, :sub_id, :region, :cloud
@@ -26,6 +26,14 @@ module ForemanAzureRm
     initializer "foreman_azure_rm.add_rabl_view_path" do
       Rabl.configure do |config|
         config.view_paths << ForemanAzureRm::Engine.root.join('app', 'views')
+      end
+    end
+
+    initializer "foreman_azure_rm.zeitwerk" do
+      Rails.autoloaders.each do |loader|
+        loader.ignore(
+          ForemanAzureRm::Engine.root.join('lib/foreman_azure_rm/version.rb')
+        )
       end
     end
 
