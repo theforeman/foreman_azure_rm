e7f3b3d2c7d068196356bdf593384be89e5dff83
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-12-10 23:39:33 +0100

    Rails 7 compatible Zeitwerk loading
    Fixes: 61433f5d07da01fdfe62b45682c9fc5dc17dbb60 ("Fixes #37625 - Fix the plugin to support zeitwerk")

--- test/unit/azure_sdk_adapter_test.rb
+++ test/unit/azure_sdk_adapter_test.rb
@@ -1,25 +1,23 @@
 require_relative '../test_plugin_helper'
 
-module ForemanAzureRm
-  class AzureSdkAdapterTest < ActiveSupport::TestCase
-    setup do
-      compute_resource = FactoryBot.build(:azure_rm)
-      tenant           = compute_resource.uuid
-      app_ident        = compute_resource.app_ident
-      secret_key       = compute_resource.password
-      sub_id           = compute_resource.user
-      cloud            = compute_resource.cloud
-      @test_adapter    = AzureSdkAdapter.new(tenant, app_ident, secret_key, sub_id, cloud)
-      AzureSdkAdapter.stubs(:gallery_caching).with('test_rg').returns({})
-    end
+class AzureSdkAdapterTest < ActiveSupport::TestCase
+  setup do
+    compute_resource = FactoryBot.build(:azure_rm)
+    tenant           = compute_resource.uuid
+    app_ident        = compute_resource.app_ident
+    secret_key       = compute_resource.password
+    sub_id           = compute_resource.user
+    cloud            = compute_resource.cloud
+    @test_adapter    = ForemanAzureRm::AzureSdkAdapter.new(tenant, app_ident, secret_key, sub_id, cloud)
+    ForemanAzureRm::AzureSdkAdapter.stubs(:gallery_caching).with('test_rg').returns({})
+  end
 
-    test "should call #actual_gallery_image_id when #gallery_caching is {} otherwise return #gallery_caching" do
-      @test_adapter.expects(:actual_gallery_image_id).with('test_rg', 'test_gallery_image_name').once.returns('test_gallery_img_id')
-      actual1 = @test_adapter.fetch_gallery_image_id('test_rg', 'test_gallery_image_name')
-      actual2 = @test_adapter.fetch_gallery_image_id('test_rg', 'test_gallery_image_name')
+  test "should call #actual_gallery_image_id when #gallery_caching is {} otherwise return #gallery_caching" do
+    @test_adapter.expects(:actual_gallery_image_id).with('test_rg', 'test_gallery_image_name').once.returns('test_gallery_img_id')
+    actual1 = @test_adapter.fetch_gallery_image_id('test_rg', 'test_gallery_image_name')
+    actual2 = @test_adapter.fetch_gallery_image_id('test_rg', 'test_gallery_image_name')
 
-      assert_equal actual1, actual2
-      assert_equal 'test_gallery_img_id', actual1
-    end
+    assert_equal actual1, actual2
+    assert_equal 'test_gallery_img_id', actual1
   end
 end
